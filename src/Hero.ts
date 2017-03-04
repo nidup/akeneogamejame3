a74da095156a29e1aee016bd654879a6528d0474
Nicolas Dupont <nicolas@akeneo.com>
2017-03-04 22:14:55 +0100

    Create Hero

---
+++ src/Hero.ts
@@ -0,0 +1,63 @@
+
+export class Hero extends Phaser.Sprite {
+
+    private cursorKeys: Phaser.CursorKeys;
+    private jumpingKey: Phaser.Key;
+    private jumpTimer = 0;
+    private facing = 'right';
+
+    constructor(game: Phaser.Game, x: number, y: number, key: string, frame: number, keyboard: Phaser.Keyboard) {
+        super(game, x, y, key, frame);
+
+        this.anchor.setTo(.5,.5);
+        game.physics.enable(this, Phaser.Physics.ARCADE);
+
+        this.body.bounce.y = 0.2;
+        this.body.collideWorldBounds = true;
+        this.animations.add('idle-left', [23], 10, true);
+        this.animations.add('left', [23, 24, 25, 26], 10, true);
+        this.animations.add('idle-right', [0], 10, true);
+        this.animations.add('right', [0, 1, 2, 3], 10, true);
+
+        game.add.existing(this);
+
+        this.cursorKeys = keyboard.createCursorKeys();
+        this.jumpingKey = keyboard.addKey(Phaser.KeyCode.SPACEBAR);
+    }
+
+    public update ()
+    {
+        this.body.velocity.x = 0;
+
+        if (this.cursorKeys.left.isDown) {
+            this.body.velocity.x = -150;
+
+            if (this.facing != 'left') {
+                this.animations.play('left');
+                this.facing = 'left';
+            }
+
+        } else if (this.cursorKeys.right.isDown) {
+            this.body.velocity.x = 150;
+
+            if (this.facing != 'right') {
+                this.animations.play('right');
+                this.facing = 'right';
+            }
+
+        } else {
+            if (this.facing == 'right' && this.facing != 'idle') {
+                this.animations.play('idle-right');
+                this.facing = 'idle';
+            } else if (this.facing == 'left' && this.facing != 'idle') {
+                this.animations.play('idle-left');
+                this.facing = 'idle';
+            }
+        }
+
+        if (this.jumpingKey.isDown && this.body.onFloor() && this.game.time.now > this.jumpTimer) {
+            this.body.velocity.y = -150;
+            this.jumpTimer = this.game.time.now + 750;
+        }
+    }
+}
--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import {MapChunkRegistry, MapChunk} from "./MapGenerator";
 import {Ship} from "./Ship";
 import {ShipBuilder} from "./ShipBuilder";
 import {KeyboardControlEngine, GamePadControlEngine, DummyControlEngine} from "./ControlEngine";
+import {Hero} from "./Hero";
 
 class SimpleGame {
     private game: Phaser.Game;
@@ -20,13 +21,9 @@ class SimpleGame {
     private enemy: Ship = null;
     */
 
+    private hero: Hero;
     private map;
     private layer;
-    private player;
-    private facing = 'right';
-    private jumpTimer = 0;
-    private cursors;
-    private jumpButton;
     private bg;
 
     constructor(config: Configuration) {
@@ -38,11 +35,10 @@ class SimpleGame {
             "content",
             this
         );
-        //this.chunkRegistry = new MapChunkRegistry(this.game.rnd, this.configuration);
     }
 
-    public preload() {
-
+    public preload()
+    {
         this.game.load.tilemap('level1', 'assets/forrest/level1.json', null, Phaser.Tilemap.TILED_JSON);
         this.game.load.image('tiles-1', 'assets/forrest/tiles.png');
         this.game.load.spritesheet('nude', 'assets/forrest/nude.png', 32, 32);
@@ -53,138 +49,30 @@ class SimpleGame {
         this.game.load.image('background-night', 'assets/forrest/background-night.png');
     }
 
-    public create() {
+    public create()
+    {
         this.createWorld();
     }
 
-    public update() {
-
-        this.game.physics.arcade.collide(this.player, this.layer);
-
-        this.player.body.velocity.x = 0;
-
-        if (this.cursors.left.isDown)
-        {
-            this.player.body.velocity.x = -150;
-
-            if (this.facing != 'left')
-            {
-                this.player.animations.play('left');
-                this.facing = 'left';
-            }
-        }
-        else if (this.cursors.right.isDown)
-        {
-            this.player.body.velocity.x = 150;
-
-            if (this.facing != 'right')
-            {
-                this.player.animations.play('right');
-                this.facing = 'right';
-            }
-        }
-        else
-        {
-            if (this.facing == 'right' && this.facing != 'idle')
-            {
-                this.player.animations.play('idle-right');
-                this.facing = 'idle';
-            }
-            else if (this.facing == 'left' && this.facing != 'idle')
-            {
-                this.player.animations.play('idle-left');
-                this.facing = 'idle';
-            }
-        }
-
-        if (this.jumpButton.isDown && this.player.body.onFloor() && this.game.time.now > this.jumpTimer)
-        {
-            this.player.body.velocity.y = -150;
-            this.jumpTimer = this.game.time.now + 750;
-        }
-
-        /*
-        // TODO: reset bullets positions when changing chunk
-        // TODO: reset enemy does not work properly, only works when enemy is following the player
-        if (this.generating === false && this.player.getX() > this.configuration.getRightBorder()) {
-            this.generating = true;
-            this.currentChunk = this.chunkRegistry.getRight(this.currentChunk);
-            this.repaintCurrentChunk();
-            let playerX = this.configuration.getLeftBorder();
-            let enemyX = playerX + (Math.abs(this.enemy.getX()) - Math.abs(this.player.getX()));
-            this.player.resetPosition(playerX, this.player.getY());
-            this.enemy.resetPosition(enemyX, this.enemy.getY());
-            this.generating = false;
-
-        } else if (this.generating === false && this.player.getX() < this.configuration.getLeftBorder()) {
-            this.generating = true;
-            this.currentChunk = this.chunkRegistry.getLeft(this.currentChunk);
-            this.repaintCurrentChunk();
-            let playerX = this.configuration.getRightBorder();
-            let enemyX = playerX + (Math.abs(this.enemy.getX()) - Math.abs(this.player.getX()));
-            this.player.resetPosition(playerX, this.player.getY());
-            this.enemy.resetPosition(enemyX, this.enemy.getY());
-            this.generating = false;
-
-        } else if (this.generating === false && this.player.getY() > this.configuration.getBottomBorder()) {
-            this.generating = true;
-            this.currentChunk = this.chunkRegistry.getBottom(this.currentChunk);
-            this.repaintCurrentChunk();
-            let playerY = this.configuration.getTopBorder();
-            let enemyY = playerY + (Math.abs(this.enemy.getY()) - Math.abs(this.player.getY()));
-            this.player.resetPosition(this.player.getX(), playerY);
-            this.enemy.resetPosition(this.enemy.getX(), enemyY);
-            this.generating = false;
-
-        } else if (this.generating === false && this.player.getY() < this.configuration.getTopBorder()) {
-            this.generating = true;
-            this.currentChunk = this.chunkRegistry.getTop(this.currentChunk);
-            this.repaintCurrentChunk();
-            let playerY = this.configuration.getBottomBorder();
-            let enemyY = playerY + (Math.abs(this.enemy.getY()) - Math.abs(this.player.getY()));
-            this.player.resetPosition(this.player.getX(), playerY);
-            this.enemy.resetPosition(this.enemy.getX(), enemyY);
-            this.generating = false;
-        }
-        */
-
-        // TODO: set a timer to pre-generate upcoming chunks to reduce the lag effect (only repaint)
+    public update()
+    {
+        this.game.physics.arcade.collide(this.hero, this.layer);
+        this.hero.update();
     }
 
-    public render() {
+    public render()
+    {
         this.game.debug.text(
             "FPS: "  + this.game.time.fps + " "
-            + " Player PV " + this.player.health + " ",
+            + " Player PV " + this.hero.health + " ",
             2,
             14,
             "#00ff00"
         );
     }
 
-    private createWorld() {
-
-        /*
-        this.game.time.advancedTiming = true;
-        this.game.world.setBounds(0, 0, this.configuration.getMapChunkWidth(), this.configuration.getMapChunkHeight());
-        this.game.physics.startSystem(Phaser.Physics.P2JS);
-        this.game.physics.p2.setImpactEvents(true);
-        this.game.physics.p2.restitution = 0.8;
-
-        this.map = this.game.add.tilemap();
-        this.map.addTilesetImage(
-            "tileset",
-            "tileset",
-            this.configuration.getTileWidth(),
-            this.configuration.getTileHeight()
-        );
-
-        this.currentChunk = this.chunkRegistry.getInitial();
-        this.repaintCurrentChunk();
-
-        this.buildPlayer();
-        this.buildEnemy();
-        */
-
+    private createWorld()
+    {
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
         this.game.stage.backgroundColor = '#000000';
@@ -223,27 +111,8 @@ class SimpleGame {
 
         this.game.physics.arcade.gravity.y = 350;
 
-        this.player = this.game.add.sprite(50, 370, 'king');
-        this.player.anchor.setTo(.5,.5);
-
-        this.game.physics.enable(this.player, Phaser.Physics.ARCADE);
-
-        this.player.body.bounce.y = 0.2;
-        this.player.body.collideWorldBounds = true;
-        //this.player.body.setSize(16, 16, 0, 0); // TODO ????
-        //this.player.body.setCircle(14); // TODO ????
-
-        this.player.animations.add('idle-left', [23], 10, true);
-        this.player.animations.add('left', [23, 24, 25, 26], 10, true);
-        this.player.animations.add('idle-right', [0], 10, true);
-        this.player.animations.add('right', [0, 1, 2, 3], 10, true);
-        //this.player.animations.add('turn', [4], 20, true);
-
-        this.game.camera.follow(this.player);
-
-        this.cursors = this.game.input.keyboard.createCursorKeys();
-        this.jumpButton = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-
+        this.hero = new Hero(this.game, 50, 370, 'king', 0, this.game.input.keyboard);
+        this.game.camera.follow(this.hero);
     }
 /*
     private buildPlayer() {
@@ -314,24 +183,6 @@ class SimpleGame {
         return this.layer;
     }*/
 }
-
-/**
- * Paints a tile map layer with the given set of tiles, the layer will contains empty tiles around the painted tiles to
- * allow to always keep the player centered in the screen
- *
-class TilemapPainter {
-    public paint(map: Phaser.Tilemap, layer: Phaser.TilemapLayer, tiles: Array<Array<number>>) {
-
-        let nbColumns = tiles.length;
-        let nbRows = tiles[0].length;
-
-        for (let column = 0; column < nbColumns; column++) {
-            for (let row = 0; row < nbRows; row++) {
-                map.putTile(tiles[column][row], column, row, layer);
-            }
-        }
-    }
-}*/
 
 window.onload = () => {
     let configuration = new Configuration();
